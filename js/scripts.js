426a51558db22e4188c5b076686f9107ab7dc8ba
zydridas <zoetsullivan@gmail.com>
2021-10-31 15:18:25 -0700

    div boxes added
    Co-authored-by: Brit Wallace britwallace888@gmail.com 
    Co-authored-by: Julien Leonardo julien.m.leonardo@gmail.com 
    Co-authored-by: Branden Clauson clauson.branden@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  $("button#hello").click(function() {
-    $("ul").prepend("<li>Hello!</li>");
+  $(".clickable").click(function() {
+    $("#js-showing").toggle();
+    $("#js-hidden").toggle();
   });
-
-  $("button#goodbye").click(function() {
-    $("ul").prepend("<li>Goodbye!</li>");
-  });
-
-  $("button#stop").click(function() {
-    $("ul").prepend("<li>Stop copying me!</li>");
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#fun-showing").toggle();
+    $("#fun-hidden").toggle();
   });
 });
